99a872c83ecfcf9ea54af3147a010b2aab990d80
Deepan <deepandeepsi02@gmail.com>
2022-03-02 18:03:34 +0530

    Added one console statement

--- Helloapp/Program.cs
+++ Helloapp/Program.cs
@@ -7,6 +7,7 @@ namespace Helloapp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Welcome to Git");
         }
     }
 }
