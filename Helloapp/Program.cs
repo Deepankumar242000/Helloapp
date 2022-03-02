c85995d6f31ccf0a86ceddf6ed04d3c205a57b8f
Deepan <deepandeepsi02@gmail.com>
2022-03-02 17:58:57 +0530

    Add project files.

---
+++ Helloapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Helloapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
